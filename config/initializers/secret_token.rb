05d31ad154de94dae9d88a7254fc39b788223b63
cristheinz <cristheinz@gmail.com>
2014-03-24 20:30:20 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrdSiteGsf::Application.config.secret_token = '27a886b867fa1f550bd740f889daeb463d0c39bb4684fe48c3b6368f54ff690beb949ead69d14b7963f2ae06929f5b857a3859cbd2646c5a01007d03bb37df36'
